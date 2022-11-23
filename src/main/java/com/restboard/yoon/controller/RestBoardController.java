290ffa0966873e401016f51a02d695876624fb40
yhw0104 <yhw0104@naver.com>
2022-11-23 12:59:40 +0900

    4th commit

--- src/main/java/com/restboard/yoon/controller/RestBoardController.java
+++ src/main/java/com/restboard/yoon/controller/RestBoardController.java
@@ -8,6 +8,10 @@ import com.restboard.yoon.service.BoardService;
 import com.restboard.yoon.dto.BoardDto;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @AllArgsConstructor
@@ -16,18 +20,40 @@ import java.util.List;
 public class RestBoardController {
     private BoardService boardService;
 
-    // @PostMapping은 url의 주소를 숨기고 데이터 전달
+    // @PostMapping은 url의 주소를 숨기고 서버에 데이터 전달
     // js로 연결시켜 다음 페이지로 넘길 수 있게 만든다. (ajax사용)
     // @RequestMapping(value = "/post", method = RequestMethod.POST)처럼 사용 가능
     // 데이터 송신을 위한 url이라 눈에 안보임 ( write.html 의 등록 버튼 누르면 /post/save url에 데이터가 전달된다.)
+    // 게시글 저장
     @PostMapping("/save")
     public Long write(@RequestBody BoardDto boardDto) {
         return boardService.savePost(boardDto);
     }
 
+    
+    // @GetMapping은 페이지에 데이터를 받아옴
+    // 게시글 목록
     @GetMapping("/list")
     public List<BoardDto> list() {
         List<BoardDto> boardList = boardService.getBoardlist();
         return boardList;
-    } 
+    }
+    
+    // 게시글 상세조회 페이지
+    @GetMapping("/post/{no}")
+    public BoardDto detail(@PathVariable("no") Long no) {
+        BoardDto boardDTO = boardService.getPost(no);
+        
+        return boardDTO;
+    }
+
+    // 여기
+    // 게시글 수정 페이지
+    @GetMapping("/post/edit/{no}")
+    public BoardDto edit(@PathVariable("no") Long no) {
+        BoardDto boardDto = boardService.getPost(no);
+
+        return boardDto;
+    }
+
 }
